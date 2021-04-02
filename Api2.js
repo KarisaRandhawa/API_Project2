9009906817c8345278b518933171ced42832184b
Karisa Randhawa <karisarandhawa@icloud.com>
2021-04-01 21:21:23 -0400

    02/04/2021

--- Api2.js
+++ Api2.js
@@ -1,63 +1,53 @@
-const baseURL ="https://calendarific.com/api/v2/holidays";
+const baseURL = "https://calendarific.com/api/v2/holidays";
 const key = "23a3878ec0db3c3bdede20150d8e532b927d7d02";
 let url;
 
 const searchTerm = document.querySelector("#search");
 const searchForm = document.querySelector("form");
 const holidayYear = document.querySelector(".holidayYear");
-const countryOfHoliday = document.querySelector(".countryOfHoliday")
+const countryOfHoliday = document.querySelector(".countryOfHoliday");
 
-const section = document.querySelector("section");
+const section = document.querySelector("#resultSection");
 searchForm.addEventListener("submit", fetchResults);
 
 function fetchResults(event) {
-    event.preventDefault ();
-    console.log(searchTerm);
-    url = `${baseURL}?api_key=${key}&country=${countryOfHoliday.value}&year=${holidayYear.value}`;
-    //console.log(url);
-    fetch (url, {
-        method: "GET",
-    })
+  event.preventDefault();
+  console.log(searchTerm);
+  url = `${baseURL}?api_key=${key}&country=${countryOfHoliday.value}&year=${holidayYear.value}`;
+  //console.log(url);
+  fetch(url, {
+    method: "GET",
+  })
     .then((results) => {
-        return results.json();
+      return results.json();
     })
     .then((json) => {
-        displayResults(json);
+      displayResults(json);
     });
 }
 
 function displayResults(results) {
-    let holidayResults = results.response.holidays;
-    for (i = 0; i <= 20; i++) {
-        console.log(holidayResults[i]);
-        let festival = holidayResults[i];
-        section.style.display = "block";
+  let holidayResults = results.response.holidays;
+  for (i = 0; i <= 10; i++) {
+    console.log(holidayResults[i]);
+    let festival = holidayResults[i];
 
-        let searchResults = document.createElement("div");
-        let name = document.createElement("h3");
-        let date = document.createElement("h5")
-        let type = document.createElement("h6");
-        let locations = document.createElement("h6");
-        let description = document.createElement("p");
-        let card = document.createElement("div")
+    let searchResults = document.createElement("div");
+    let name = document.createElement("h4");
+    let date = document.createElement("h5");
+    let type = document.createElement("h5");
+    let description = document.createElement("h6");
 
+    searchResults.setAttribute("class", "card");
+    name.textContent = festival.name;
+    date.innerHTML = `<h5> Celebrated on: ${festival.date.iso}</h5>`;
+    type.innerHTML = `<h5> Type of Holiday: ${festival.type}</h5>`;
+    description.textContent = festival.description;
 
-
-        name.textContent = festival.name;
-        date.innerHTML = `<h5> Celebrated on: ${festival.date.iso}</h5>`;
-        type.innerHTML = `<h6> Type of Holiday: ${festival.type}</h6>`;
-        locations.innerHTML = `<h6> Celebrations Take Place in: ${festival.locations}</h6>`;
-        description.textContent = festival.description;
-
-        // card.setAttribute("class", "card");
-
-        searchResults.appendChild(name);
-        searchResults.appendChild(date);
-        searchResults.appendChild(type);
-        searchResults.appendChild(locations);
-        searchResults.appendChild(description);
-        section.appendChild(searchResults);
-
-    }
+    searchResults.appendChild(name);
+    searchResults.appendChild(date);
+    searchResults.appendChild(type);
+    searchResults.appendChild(description);
+    section.appendChild(searchResults);
+  }
 }
-
